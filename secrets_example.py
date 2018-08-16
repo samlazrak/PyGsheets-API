01c3d702dc90732f8bce7490699af95a3a1cd459
Sam Lazrak <24284329+samlazrak@users.noreply.github.com>
2018-08-16 17:29:17 -0500

    Create secrets_example.py

---
+++ secrets_example.py
@@ -0,0 +1,16 @@
+# Scope of the Google API that this application will have access to.
+# Read more here: https://developers.google.com/sheets/api/guides/authorizing
+scope = "https://www.googleapis.com/auth/spreadsheets"
+
+# Google Drive folder id
+python_folder = ""
+
+# Sheets id
+spreadsheet_id = ""
+
+# The ranges of rows and columns this application has access to.
+# Example: Sheet1!A1:A4
+range_name = ""
+
+# Option for how data is parsed, RAW or USER_ENTERED
+value_input_option = ""
