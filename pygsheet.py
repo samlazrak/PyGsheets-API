1e7f98548e114815c85510dea5f1630a9e32e24a
samlazrak <samlazrak@outlook.com>
2018-08-16 04:12:52 -0500

    basic writing

--- pygsheet.py
+++ pygsheet.py
@@ -1,18 +1,42 @@
 from __future__ import print_function
+
 from googleapiclient.discovery import build
 from httplib2 import Http
-from oauth2client import file, client, tools
+from oauth2client import client, file, tools
 
-import secrets
+from secrets import SCOPES, spreadsheet_id, value_input_option
+
+# Authorization
 
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('credentials.json', secrets.SCOPES)
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
-result = service.spreadsheets().values().get(spreadsheetId=secrets.SPREADSHEET_ID, range=secrets.RANGE_NAME).execute()
-numRows = result.get('values') if result.get('values') is not None else 0
-print('{0} rows retrieved'.format(numRows))
-
+# Functions
+data = [
+    {
+        "range": "Sheet1!A1:A4",
+        "majorDimension": "COLUMNS",
+        "values": [
+            ["Item", "Wheel", "Door", "Engine"]
+        ]
+    },
+    {
+        "range": "Sheet1!B1:D2",
+        "majorDimension": "ROWS",
+        "values": [
+            ["Cost", "Stocked", "Ship Date"],
+            ["$20.50", "4", "3/1/2018"]
+        ]
+    }
+]
+body = {
+    'valueInputOption': value_input_option,
+    'data': data
+}
+result = service.spreadsheets().values().batchUpdate(
+    spreadsheetId=spreadsheet_id, body=body).execute()
+print('{0} cells updated.'.format(result.get('updatedCells')));
